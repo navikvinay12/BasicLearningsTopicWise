92f9f24e34f393eca44790a09fee5c4f9513418b
Vinay <navik.vinay12@gmail.com>
2022-11-28 23:56:59 +0530

    program for Default arguments

---
+++ Demo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingPractices
+{
+    public class Demo
+    {
+        private int a, b, c;
+        public void SetValue(int X, int Y = 10, int Z = 20)
+        {
+            a = X;
+            b = Y;
+            c = Z;
+        }
+        public void PrintValue()
+        {
+            Console.WriteLine($"Values are : {a},{b},{c}");
+        }
+    }
+}
+/*C#.Net has the concept of Default Arguments, which are also known as Optional Arguments in C#.
+
+Understand the concept of Default Arguments by these points:
+
+Every default argument contains a default value within the function definition.
+If we do not pass any argument for default argument then, it uses default value.
+Given default value for default argument must be a constant.
+Default argument cannot be used for constructor and indexer etc.*/
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingPractices
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Demo D = new Demo();
+
+            D.SetValue(5);
+            D.PrintValue();
+
+            D.SetValue(5, 8);
+            D.PrintValue();
+
+            D.SetValue(5, 8, 13);
+            D.PrintValue();
+        }
+    }
+}
