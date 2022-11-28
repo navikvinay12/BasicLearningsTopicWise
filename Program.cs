ce0049602c25630135ff95aeded42485eb585ec2
Vinay <navik.vinay12@gmail.com>
2022-11-28 21:40:22 +0530

    demonstrate the class and object creation

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TestingPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample S1 = new Sample();
+            S1.Print();
+
+            Sample S2= new Sample();
+            S2.read();
+            S2.Print();
+        }
+    }
+}
---
+++ Sample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingPractices
+{
+    public class Sample
+    {
+        private int X;
+        private int Y;
+        public Sample()
+        {
+            X = 5;
+            Y = 10;
+        }
+        public void read()
+        {
+            Console.WriteLine("Enter value of X: ");
+            X = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter value of Y: ");
+            Y= Convert.ToInt32(Console.ReadLine());
+        }
+        public void Print()
+        {
+            Console.WriteLine("Value of X : " + X);
+            Console.WriteLine("Value of Y : " + Y);
+        }
+    }
+}
