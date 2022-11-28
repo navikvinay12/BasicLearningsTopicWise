ea8a924f16c3af3466097d4aa7a6fecd8190b60d
Vinay <navik.vinay12@gmail.com>
2022-11-28 22:04:02 +0530

    this keyword concept

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingPractices
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Sample S;
+            S = new Sample();
+            S.SetValues(10, 20);    
+        }
+    }
+}
---
+++ Sample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingPractices
+{
+    public class Sample
+    {
+        private int a;
+        private int b;
+        public Sample() 
+        {
+            a = 1;
+            b = 2; 
+        }
+        public void SetValues(int a,int b)
+        {
+            Console.WriteLine("without using this keyword : "+a);   //scope is limited to local method
+            Console.WriteLine("without using this keyword : " + b); //scope is limited to local method
+            Console.WriteLine("using this keyword : " + this.a);    //using this keyword it is accessing current class instance variable
+            Console.WriteLine("using this keyword : " + this.b);    //using this keyword it is accessing current class instance variable
+
+        }
+    }
+}
