91ea631dd13ad3df55358f6097adedc314e6d099
Vinay <navik.vinay12@gmail.com>
2022-11-29 00:33:42 +0530

    pass object as argument into method

--- Program.cs
+++ Program.cs
@@ -10,7 +10,19 @@ namespace Testing
     {
         public static void Main(string[] args)
         {
-
+            //Object Creation
+            Sample S1=new Sample();
+            Sample S2=new Sample();
+            Sample S3= new Sample();
+            //passing integers
+            S1.SetValue(10);
+            S2.SetValue(20);
+            //passing objects
+            S3.AddObj(S1, S2);
+            //printing the Objects
+            S1.PrintValue();
+            S2.PrintValue();
+            S3.PrintValue();
         }
     }
 }
---
+++ Sample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class Sample
+    {
+        private int value;
+        public void SetValue(int v)
+        {
+            value = v;
+        }
+        public void PrintValue()
+        {
+            Console.WriteLine("Value : "+ value);
+        }
+        public void AddObj(Sample S1,Sample S2)
+        {
+            value= S1.value+S2.value;
+        }
+    }
+}
+//As we know that we can pass primitive (basic) data types in methods as arguments. 
+//    Similarly, we can pass objects in methods too.
